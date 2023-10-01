d744353afd440df515ffbae2ecbfa6cf5f90dce6
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-10-01 10:10:38 +0700

    update project

--- src/data/project.ts
+++ src/data/project.ts
@@ -1,24 +1,22 @@
-import image from './../assets/images/Sugar Cream.png';
-
+import landingPage from './../assets/images/landing page.png';
+import shopManagement from './../assets/images/shop management.png';
 export const projects = [
   {
     id: 1,
-    name: 'OneMobile',
-    image: image,
+    name: 'Trending blockchain landing page',
+    image: landingPage,
     github_link: 'https://github.com/khangle23078/OneMobile-backend',
     demo_link: 'https://github.com/khangle23078/OneMobile-backend',
-    description:
-      'TV application, displays different categories of movies/shows (popular, in theaters, trending etc) alongside a search option using TheMovieDB API for the data',
-    tech_stack: 'React - Redux - Typescript - Tailwindcss',
+    description: 'Website intoduction about blockchain landing page',
+    tech_stack: 'React - Vite - Typescript - Tailwindcss',
   },
   {
     id: 2,
     name: 'OneMobile',
-    image: image,
+    image: shopManagement,
     github_link: 'https://github.com/khangle23078/OneMobile-backend',
     demo_link: 'https://github.com/khangle23078/OneMobile-backend',
-    description:
-      'TV application, displays different categories of movies/shows (popular, in theaters, trending etc) alongside a search option using TheMovieDB API for the data',
-    tech_stack: 'React - Redux - Typescript - Tailwindcss',
+    description: 'Website about shop management like product,category and order',
+    tech_stack: 'Angular - Typescript - Rxjs - Ngzoro',
   },
 ];
